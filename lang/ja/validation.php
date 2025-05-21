3772a17616e892b7e1f646ddd6a1776d1f7c8eae
Atsusan <as2k.sugimoto@gmail.com>
2025-05-21 17:10:35 +0900

    initinalSetting

---
+++ lang/ja/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'accepted'             => ':attribute を承認してください。',
+    'active_url'           => ':attribute は有効なURLではありません。',
+    'after'                => ':attribute には :date より後の日付を指定してください。',
+    'alpha'                => ':attribute には英字のみ使用できます。',
+    'required'             => ':attribute は必須項目です。',
+    'email'                => ':attribute には有効なメールアドレスを指定してください。',
+    'unique'               => ':attribute はすでに存在しています。',
+    // 追加のメッセージも必要に応じて書き足してください
+
+    // 属性名のカスタマイズ
+    'attributes' => [
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+        'name' => '名前',
+    ],
+];
